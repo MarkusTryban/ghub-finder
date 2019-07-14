c6da382e3b52370eda0ead402a11eda57ea0de61
MarkusTryban <markustryban@gmail.com>
2019-07-14 18:45:38 -0400

    Added repos & repoItem component & data

--- src/App.js
+++ src/App.js
@@ -13,21 +13,10 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const res = await axiox.get(
-  //     `https://api.github.com/users?client_id=${
-  //       process.env.REACT_APP_GITHUB_CLIENT_ID
-  //     }&clent_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-
-  //   this.setState({ users: res.data, loading: false });
-  // }
 
   // Search Github users
   searchUsers = async text => {
@@ -55,6 +44,19 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  //Get user repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axiox.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&clent_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -65,7 +67,7 @@ class App extends Component {
     setTimeout(() => this.setState({ alert: null }), 5000);
   };
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -97,7 +99,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
